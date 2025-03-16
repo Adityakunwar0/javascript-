159d1d21a67d16f83cae1185974a4ecf1940ea6c
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 12:50:09 +0000

    conversion

---
+++ 01_Basic/conversion.js
@@ -0,0 +1,29 @@
+let score = "Aditya"
+
+console.log(typeof score); // string
+console.log(typeof(score)); // string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // nan => not a number
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Aditya";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aditya" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
