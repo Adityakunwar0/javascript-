91d81ee5c597ba5945c68428c9727eaa292a907a
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 13:07:28 +0000

    comparison

---
+++ 01_Basic/comparison.js
@@ -0,0 +1,27 @@
+// //comparison operator
+
+// let d=5;
+// let e="5";
+
+// console.log(d==e); // only compare number
+// console.log(d===e); // compare number and data type
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+// avoid these type of compare
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+ 
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
