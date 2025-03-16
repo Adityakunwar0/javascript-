5450643b77e18284ee46a321dce54c624f7be09f
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 16:19:54 +0000

    string

---
+++ 01_Basic/string1.js
@@ -0,0 +1,30 @@
+const name = "Aditya"
+const repoCount = 50;
+
+console.log (`Hello my name is ${name} and my repocount is ${repoCount} `);
+
+// const myName = new String("Aditya");
+// console.log(myName[0]); // A
+// console.log(myName.__proto__); // {}
+// console.log(myName.charAt(2)); // i
+// console.log(myName.indexOf('t')); //3
+
+// console.log(myName.length); //6
+
+// const newString = myName.substring(0, 4);
+// console.log(newString); // Adit
+
+// const anotherString = myName.slice(-6, 4);
+// console.log(anotherString); // Adit
+
+// const newStringOne = "   Aditya   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); // no whitespaces 
+
+// const url = "https://Aditya.com/Aditya%2kunwar";
+
+// console.log(url.replace('%2', '-'));  
+// console.log(url.includes('Aditya')); // true
+
+const myName = new String("Aditya-tyu-trhyt");
+console.log(myName.split('-')); // convert string into array 
