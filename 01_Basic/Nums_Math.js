b882482a41d56ff877125fab478c53d958490b25
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 16:45:12 +0000

    Nums_Math

---
+++ 01_Basic/Nums_Math.js
@@ -0,0 +1,35 @@
+// const score = 400
+// console.log(score); //400
+
+// const balance =  new Number(100);
+// console.log(balance); //[Number: 100]
+
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(1)); // 100.0
+
+// const otherNumber = 23.65788;
+// console.log(otherNumber.toPrecision(3)); // 23.7
+
+// const hundreds = 122568987
+// console.log(hundreds.toLocaleString('en-IN')); 12,25,68,987
+
+//-----------maths-----------
+
+// console.log(Math); // object [math] {}
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(7689.785)); //7690
+// console.log(Math.ceil(78.7)); //79
+// console.log(Math.floor(89.78)); //89
+// console.log(Math.min(5,8,6,3,9)); // 3
+// console.log(Math.max(5,8,6,3,9)); // 9
+
+console.log(Math.random()); // random value from 0 to 1
+console.log(Math.random()*10); // random value from 0 to 9
+
+console.log(Math.floor(Math.random()*10) + 1); // smallest value from 1 to 9
+
+const min =10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // range from min and max value add min value 
+
+
