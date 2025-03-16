5627e98b1f5c5f4a2337760a957274fe30ebef2f
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 15:57:12 +0000

    Datatype_Summary

--- 01_Basic/Datatype_Summary.js
+++ 01_Basic/Datatype_Summary.js
@@ -1,34 +1,56 @@
-// primitive data type : call by value
-// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// // primitive data type : call by value
+// // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
-// dynamical type data type is javascript
+// // dynamical type data type is javascript
 
 
 
-const score = 100;
-const scoreValue = 100.3;
-const isLoggedIn = false;
-const outsideTemp = null; // ==> type of objects
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null; // ==> type of objects
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol('123');
-const anotherId = Symbol('123');
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
 
-console.log(id === anotherId); // false
+// console.log(id === anotherId); // false
 
-const BigInt = 32467876347898965696557780n;
+// const BigInt = 32467876347898965696557780n;
 
-// Reference ( Non primitive )
-// Array, objects, functions
+// // Reference ( Non primitive )
+// // Array, objects, functions
 
-const heroes = ["shaktiman", "naagraaj", "doga"]
-let myObj = {
-    name: "Aditya",
-    age: 24
+// const heroes = ["shaktiman", "naagraaj", "doga"]
+// let myObj = {
+//     name: "Aditya",
+//     age: 24
+// }
+
+
+// const myFunction = function(){
+//     console.log("hello world"); // ==> type of object function
+// }
+
+// stack memory : Primitive, Heap (non-primitive)
+
+let myName = "Aditya";
+
+let anothername = myName;
+anothername = "Chotu";
+
+
+console.log(myName);
+console.log(anothername);
+
+let userOne = {
+    email: "Aditya@gmail.com",
+    upi: "438795069@ybl"
 }
 
+let userTwo = userOne;
+userTwo.email = "chotu@gmail.com";
 
-const myFunction = function(){
-    console.log("hello world"); // ==> type of object function
-}
+console.log(userOne.email);
+console.log(userTwo.email);
