b32455ab7ae5d0a914e936029e35dbae3ab3ab54
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-16 12:37:07 +0000

    Datatype

---
+++ 01_Basic/Datatype.js
@@ -0,0 +1,13 @@
+let name = "aditya";
+let age = 23;
+let isLoggedIn = false;
+
+// number ==> 2 to power 53
+// bigInt 
+// string ==> ""
+// boolean => true/false
+// null typeof objects => Standalone value
+//undefined type of undefined =>
+//symbol => used in unique case
+
+
