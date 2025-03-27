1e43e141f99a5282c706b6f6d1237a20640a15ed
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-27 08:09:19 +0000

    global and scope variable

---
+++ 03_functions/function2.js
@@ -0,0 +1,36 @@
+// function one() {
+//     const username = "Aditya";
+//     function two(){
+//         const website = "youtube";
+//         console.log(username);
+//     }
+//    // console.log(website); // error
+//     two(); // Aditya
+
+// }
+// //one();
+if(true){
+    const username = "Aditya"
+    if (username === "Aditya"){
+        const website = " Youtube";
+        // console.log(username + website); // Aditya Youtube
+    }
+   // console.log(website); // error beacuse website scope is in inside if case
+
+}
+// console.log(username); // error
+
+
+// +++++++++++++++++ Interesting +++++++++++++++++++++
+// console.log(addone(5)); // 6
+
+// function addone(num){
+//     return num + 1
+// }
+// addone(5); // not error
+
+console.log(addTwo(5)); // Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num +2
+}
+
