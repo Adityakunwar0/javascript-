272ff64f5bea8783bfb1f6ad4e2cb73aec4176c1
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-03-27 06:36:41 +0000

    objects destructuring

---
+++ 02_Array_Basic/objects3.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "Js in Hindi",
+    price: "999",
+    courseInstrutor: "Aditya"
+}
+
+// const {courseInstrutor} = course
+// console.log(courseInstrutor); // Aditya
+
+// const {courseInstrutor: instructor} = course
+// console.log(instructor); // Aditya
+
+// const navbar = ({company}) =>{
+
+// } // using in react this is call objects destructuring
+
+
