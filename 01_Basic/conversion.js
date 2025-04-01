bc9d61f847ee67b53e0eafb3fe307aaa5cbd7c42
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2025-04-01 05:24:11 +0000

    discuss on conversion

--- 01_Basic/conversion.js
+++ 01_Basic/conversion.js
@@ -1,11 +1,46 @@
-let score = "Aditya"
+// let score = "Aditya"
 
-console.log(typeof score); // string
-console.log(typeof(score)); // string
+// console.log(typeof score); // string
+// console.log(typeof(score)); // string
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // nan => not a number
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // nan => not a number
+
+
+// conversion of DataType
+// let score = 33;
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
+
+// let valueInNumber = String(score)
+// console.log(typeof valueInNumber); // string
+// console.log(valueInNumber); // 33
+
+// let score = "33abc";
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // Number
+// console.log(valueInNumber); // Nan
+
+// let score = null;
+// console.log(typeof score); // object
+// console.log(typeof (score)); // object
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // Number
+// console.log(valueInNumber); // 0
+
+
+let score = true;
+console.log(typeof score); // boolean
+console.log(typeof (score)); // boolean
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // Number
+console.log(valueInNumber); // 1
 
 
 // "33" => 33
